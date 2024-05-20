3bef0d8c870906e0a0462f8db0100b4b58ce58fa
andreylikhterman <148850949+andreylikhterman@users.noreply.github.com>
2024-05-20 12:47:37 +0300

    Update Popup_message.js

--- scripts/Popup_message.js
+++ scripts/Popup_message.js
@@ -1,16 +1,18 @@
 function windowUp () {
   if (!sessionStorage.getItem('Closed')) {
     document.getElementById('darker').style.display = "block";
+    document.body.style.overflow = 'hidden';
   }
 }
 
 window.onload = () => {
-  setTimeout('windowUp();', 30000);
+  setTimeout('windowUp();', 5000);
 };
 
 var close = document.getElementById("closebtn");
 close.addEventListener("click", function () {
   sessionStorage.setItem('Closed', true);
   this.parentElement.style.display='none';
+  document.body.style.overflow = 'auto';
   document.body.style.backgroundColor = back_color;
 });
